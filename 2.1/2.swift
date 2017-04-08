f18dcc3e961731e71bb3f905069c5d8874b70899
hasenbalg <frankam@inbox.lv>
2017-04-08 20:44:25 +0200

    compiled with expacted error

--- 2.1/2.swift
+++ 2.1/2.swift
@@ -1,6 +1,19 @@
-function kk(){
-  let array: Int[] = []
+
+func kk(){
+  let array: Array<Int> = []
   let element: Int = 1
   array.append(element)
 }
 kk()
+/*
+https://iswift.org/playground:
+Swift:: Error: cannot use mutating member on immutable value: 'array' is a 'let'
+ constant
+  array.append(element)
+  ^~~~~
+tmp/a5A9RXHmjZ.swift:4:3: note: change 'let' to 'var' to make it mutable
+  let array: Array<Int> = []
+  ^~~
+  var
+
+*/
