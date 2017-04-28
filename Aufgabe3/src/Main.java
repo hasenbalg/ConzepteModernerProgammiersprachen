b82d8249e2f7901817bf7029acca57749022d2ce
frank <frankam@inbox.lv>
2017-04-28 11:33:20 +0200

    aufgabe 3 update

--- Aufgabe3/src/Main.java
+++ Aufgabe3/src/Main.java
@@ -1,17 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 
 public class Main {
 
 	static Iterator<Integer> enumFrom(Integer start){
-		while (true) yield return i++;
-		
+		/*die einen Iterator liefert, 
+		der startend mit dem Wert start 
+		alle Nachfolger dieser Zahl liefert.*/
+		List<Integer> iter =  new ArrayList<Integer>();
+		while(iter.size()< 100){
+			iter.add(start++);
+			
+		}
+		//Iterable<Integer> iterable = iter;
+		return iter.iterator();
 	}
 	static <T> Iterator<T> take(Iterator<T> iterator, Integer count){
-		for(Iterator<T> i : enumFrom(iterator)){
-			System.out.print(i.toString());
-		}
+		/*die einen Iterator liefert, der die 
+		ersten count Elemente des ubergebenen Iterator liefert.
+		Verwenden Sie eine while-Schleife und die zuvor 
+		definierten Iteratoren, um alle Zahlen von 1000 bis 1009 auf
+		der Konsole auszugeben. */
+		List<Integer> huhu = new ArrayList<>();
+		while(iterator.hasNext())huhu.add((Integer) iterator.next());
+		
+		return (Iterator<T>) huhu.subList(0, count+1).iterator();
+		
+		
 	}
 	
 	
@@ -24,9 +43,10 @@ public class Main {
 		for(Base b : Option.parseRNA("UACG"))System.out.println(b.toString());
 		
 		System.out.println("_____________________");		
-		for(Base b : Option.parseRNA("UACXG"))System.out.println(b.toString());
+		//for(Base b : Option.parseRNA("UACXG"))System.out.println(b.toString());
 		
-		
+		Iterator<Integer> haha = take(enumFrom(1000),9); 
+		while(haha.hasNext())System.out.println(haha.next());
 		
 	}
 }
